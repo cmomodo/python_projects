a39b9ea0ee2568c2b93737b43985c67f5a3a800e
cmomodo <ceesay.ml@outlook.com>
2022-07-13 00:52:15 +0100

    Lab activity tasks

---
+++ aws_restart/numeric_datatypes.py
@@ -0,0 +1,31 @@
+print("Python has three numeric types: int, float, and complex")
+print("example")
+
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=3.14
+print(myValue)
+
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue1=5j
+print(myValue1)
+print(type(myValue1))
+
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue2=True
+print(myValue2)
+print(type(myValue2))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+myValue3=False
+print(myValue3)
+print(str(myValue3) + " is of teh data type " + str(type(myValue3)))
